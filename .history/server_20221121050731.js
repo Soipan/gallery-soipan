47432c8fa859daf44d746b22cc16b580f0ac27bf
Soipan <sshirlene679@gmail.com>
2022-11-21 05:07:44 -0500

    mongo

---
+++ .history/server_20221121050731.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('./_config');
+
+// Define routes
+let index = require('./routes/index');
+let image = require('./routes/image');
+
+// Initializing the app
+const app = express();
+
+// connecting the database
+
+const MONGODB_URI = "mongodb+srv://soipan:L4q3G4sprXQdi3Zf@project1.mszv13b.mongodb.net/?retryWrites=true"
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+    if (err) {
+        console.log(err)
+    }else{
+        console.log(`Connected to Database: ${MONGODB_URI}`)
+    }
+});
+
+// test if the database has connected successfully
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
+
+
+
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Set up the public folder;
+app.use(express.static(path.join(__dirname, 'public')));
+
+// body parser middleware
+app.use(express.json())
+
+
+app.use('/', index);
+app.use('/image', image);
+
+
+
+ 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() =>{
+    console.log(`Server is listening at http://localhost:${PORT}`)
+});
+
+
+module.exports = app;
